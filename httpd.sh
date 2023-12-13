9a43730bd9abd4a27ad6c42af6ef00f848052f38
CaiiTa7 <98960052+CaiiTa7@users.noreply.github.com>
2023-12-13 22:16:28 +0100

    Update httpd.sh

--- httpd.sh
+++ httpd.sh
@@ -1,17 +1,18 @@
 #!/bin/bash
+apt install -y build-essential libpcre3-dev libssl-dev libapr1-dev libaprutil1-dev make cmake gcc software-properties-common ca-certificates lsb-release apt-transport-https 
 
 # Installation d'Apache HTTP Server 2.4.49 depuis les sources
 cd /tmp
-sudo wget https://archive.apache.org/dist/httpd/httpd-2.4.49.tar.gz
-sudo tar -xzvf httpd-2.4.49.tar.gz
+wget https://archive.apache.org/dist/httpd/httpd-2.4.49.tar.gz
+tar -xzvf httpd-2.4.49.tar.gz
 cd httpd-2.4.49
 
 # Configuration, compilation et installation d'Apache
 ./configure --prefix=/usr/local/apache2 --enable-mods-shared=all --enable-ssl --enable-so --enable-cgid
-sudo make
-sudo make install
-sudo rm -rf /tmp/httpd-2.4.49.tar.gz /tmp/httpd-2.4.49
+make
+make install
+rm -rf /tmp/httpd-2.4.49.tar.gz /tmp/httpd-2.4.49
 
-sudo sed -i 's/User daemon/User killua/;s/Group daemon/Group user/' /usr/local/apache2/conf/httpd.conf
-sudo sed -i '/LoadModule cgid_module modules/s/^#//g' /usr/local/apache2/conf/httpd.conf
-sudo sed -i '/<Directory "\/usr\/local\/apache2\/cgi-bin">/ { N; N; s/Options None/Options +ExecCGI/; }' /usr/local/apache2/conf/httpd.conf
+sed -i 's/User daemon/User killua/;s/Group daemon/Group user/' /usr/local/apache2/conf/httpd.conf
+sed -i '/LoadModule cgid_module modules/s/^#//g' /usr/local/apache2/conf/httpd.conf
+sed -i '/<Directory "\/usr\/local\/apache2\/cgi-bin">/ { N; N; s/Options None/Options +ExecCGI/; }' /usr/local/apache2/conf/httpd.conf
